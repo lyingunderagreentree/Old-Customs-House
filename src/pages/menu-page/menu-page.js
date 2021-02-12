c9b41cecdf4ef9bf05d73fbb0e51aaa2b512f45c
Helen <elijahsuzzivan@gmail.l.com>
2021-02-13 01:38:09 +0400

    minor fixes

--- src/pages/menu-page/menu-page.js
+++ src/pages/menu-page/menu-page.js
@@ -61,51 +61,7 @@ class MenuPage extends React.Component {
         </div>
       )
     }
-    // return (
-    //   <div className="menu-page page columns">
-    //     <Layout>
-
-    //       <div className="background background--menu is-hidden-mobile"></div>
-
-    //       <div className="container column is-10 is-10-mobile">
-
-    //         <div className="content-holder">
-    //           <div className="content-holder__item">
-
-    //             <div><h2>Меню</h2></div>
-    //             <Fade bottom cascade>
-    //             <ul className="menu-list">
-                
-    //               <li className="menu-list__item">
-    //                 <Link to='/menu-item-page' className="highlighted">Специальные блюда</Link>
-    //               </li>
-    //               <li className="menu-list__item">
-    //                 <Link to='/menu-item-page' className="highlighted">Летнее меню</Link>
-    //               </li>
-    //               <li className="menu-list__item"><Link to='/menu-item-page' className="highlighted">Меню аквариума</Link></li>
-    //               <li className="menu-list__item"><Link to='/menu-item-page' className="highlighted">Холодные закуски</Link></li>
-    //               <li className="menu-list__item"><Link to='/menu-item-page' className="highlighted">Горячие закуски</Link></li>
-    //               <li className="menu-list__item"><Link to='/menu-item-page' className="highlighted">Супы</Link></li>
-    //               <li className="menu-list__item"><Link to='/menu-item-page' className="highlighted">Горячие рыбные блюда</Link></li>
-    //               <li className="menu-list__item"><Link to='/menu-item-page' className="highlighted">Горячие мясные блюда</Link></li>
-    //               <li className="menu-list__item"><Link to='/menu-item-page' className="highlighted">Гарниры</Link></li>
-    //               <li className="menu-list__item"><Link to='/menu-item-page' className="highlighted">Десерты</Link></li>
-    //               <li className="menu-list__item"><Link to='/menu-item-page' className="highlighted">Вегетарианское меню</Link></li>                  
-    //             </ul>
-    //             </Fade>
-    //           </div>
-    //         </div>
-          
-    //         <div className="menu-pic is-hidden-mobile">
-    //           <img src={mainMenuPic} className="menu-pic__front"/>
-    //           <img src={mainMenuPicDetails} className="menu-pic__back"/>
-    //         </div>
-            
-    //       </div>
-    //     </Layout>
-
-    //   </div>
-    // )
+   
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {
-					loader: "babel-loader"
+					loader: 'babel-loader?cacheDirectory'
 				},
 			},
 			{
